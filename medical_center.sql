b053c0d9659de3ea083c10a17f796871a0ce78db
Franklin Kong <franklinkong981@gmail.com>
2024-02-26 11:41:42 -0800

    Completed schema for a medical center

---
+++ medical_center.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS medical_center;
+
+CREATE DATABASE medical_center;
+
+\c medical_center
+
+CREATE TABLE doctors
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT
+);
+
+CREATE TABLE patients
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    is_active BOOLEAN DEFAULT true
+)
+
+CREATE TABLE diseases
+(
+    id SERIAL PRIMARY KEY,
+    disease_name TEXT NOT NULL,
+    symptoms TEXT[]
+)
+
+CREATE TABLE visits
+(
+    id SERIAL PRIMARY KEY,
+    doctor_id INTEGER REFERENCES doctors ON DELETE SET NULL,
+    patient_id INTEGER REFERENCES patients,
+    diseases_id INTEGER REFERENCES diseases
+)
